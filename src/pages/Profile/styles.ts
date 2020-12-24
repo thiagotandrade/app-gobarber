05f7cfa72fc9e3db7575fbb4aae64ee2328afec2
thiagotandrade <thiagotales@gmail.com>
2020-12-24 11:20:35 -0300

    Version 1.0

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 24px 0;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 40px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  margin-top: 32px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 186px;
+  height: 186px;
+  border-radius: 98px;
+  align-self: center;
+`;
